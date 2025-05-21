c7c140bc053c3a861bb4973dd7c96e9b698d26c9
SHAMANTH-GOWDA <shamanthgowda571@gmail.com>
2025-05-21 19:20:55 +0530

    inital commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("The Addition of 3 and 4 is " + (3 + 4));
+
+    }
+}
